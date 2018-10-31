e786f1d712bd5e02b7e22e523f3ce78c83b9c526
psatler <pablosatler@gmail.com>
2018-10-31 17:42:23 -0300

    feat: create store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//store stuff
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers"; //importing the default export from index inside reducers folder
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
