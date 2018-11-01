0229ca23fb311e642c585dd9ee2b080f698e0fc0
psatler <pablosatler@gmail.com>
2018-11-01 13:24:40 -0300

    feat: create add_tweet action creator

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-### Chirper
+### Tweet-like app
+
+## Store architecture
+
+The architecture of the redux store is as follows:
+
+```js
+{
+  tweets: {
+    tweetId: { tweetId, authorId, timestamp, text, likes, replies, replyingTo},
+    tweetId: { tweetId, authorId, timestamp, text, likes, replies, replyingTo}
+  },
+  users: {
+    userId: {userId, userName, avatar, tweets array},
+    userId: {userId, userName, avatar, tweets array}
+  },
+  authedUser: userId
+}
+```
--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,34 @@
-import { saveLikeToggle } from "../utils/api";
+import { saveLikeToggle, saveTweet } from "../utils/api";
+
+//importing loading bar to show when we submit a tweet
+import { showLoading, hideLoading } from "react-redux-loading";
 
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 export const TOGGLE_TWEET = "TOGGLE_TWEET";
+export const ADD_TWEET = "ADD_TWEET";
+
+function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  };
+}
+
+//args: tweet text and the tweet that the newTweet is replying to, if any
+export function handleAddTweet(text, replyingTo) {
+  //using getState to get the current state of our store
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading()); //show loading bar
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then(tweet => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
 
 //action creator
 export function receiveTweets(tweets) {
@@ -20,7 +47,7 @@ function toggleTweet({ id, authedUser, hasLiked }) {
     hasLiked
   };
 }
-
+//assynchronous action creator (which is exported)
 export function handleToggleTweet(info) {
   return dispatch => {
     //using optimistic updates here, so first we toggle the tweet and then update the backend (server)
